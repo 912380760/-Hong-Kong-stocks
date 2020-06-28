30b25bbb32befb9005756cbd48988500fa774da3
xxxqqqeee <xxxqqqeee@yxunistar.com>
2020-06-29 02:08:21 +0800

    添加港股券商数据和打新记录,数据已测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,16 +10,40 @@
 </template>
 
 <script>
+// 配售数据
 import data from '../assets/配售数据';
+// 券商数据
+import brokerList from '../assets/brokerList';
+// 打新记录数据
+import stockList from '../assets/stockList';
+// 创建配售对象
 import CreateData from '../assets/CreateData';
+// 创建券商对象
+import CreateBroker from "../assets/CreateBroker";
 import echarts from 'echarts';
 
-
 const 配售对象 = [];
+const 券商对象 = {};
 data.forEach((ele) => {
     配售对象.push(new CreateData(ele));
 })
-console.log(配售对象)
+brokerList.forEach((ele) => {
+    券商对象[ele.name] = new CreateBroker(ele);
+});
+stockList.forEach((ele) => {
+    const 股票名称 = ele.name;
+    ele.打新记录.forEach((element) => {
+        券商对象[element.券商].添加港股打新记录(element)
+    });
+    ele.中签记录.forEach((element) => {
+        券商对象[element.券商].港股中签(element)
+    });
+    ele.出售记录.forEach((element) => {
+        券商对象[element.券商].出售中签股票(element)
+    })
+})
+
+console.log(配售对象, 券商对象)
 
 export default {
     name: 'HelloWorld',
@@ -77,12 +101,13 @@ export default {
 
             // 使用刚指定的配置项和数据显示图表。
             myChart.setOption(option);
-        }
+        },
     },
     mounted() {
         this.配售对象.forEach((ele, index) => {
             this.createChart('main' + index, ele);
-        })
+        });
+
     }
 }
 </script>
